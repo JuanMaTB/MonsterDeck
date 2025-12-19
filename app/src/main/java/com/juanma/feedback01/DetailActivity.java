9bc38afd5227336813493c486da304c68ded8d36
Juan Manuel Torrado <jm.torrado.borrachero@gmail.com>
2025-12-19 11:12:30 +0100

    Fix layout IDs mismatch in DetailActivity (build error resolved)

--- app/src/main/java/com/juanma/feedback01/DetailActivity.java
+++ app/src/main/java/com/juanma/feedback01/DetailActivity.java
@@ -1,12 +1,9 @@
 package com.juanma.feedback01;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
@@ -16,36 +13,12 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // Botón “volver” arriba (flecha)
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setTitle("Detalle");
-        }
-
         ImageView img = findViewById(R.id.detailImg);
         TextView name = findViewById(R.id.detailName);
         TextView level = findViewById(R.id.detailLevel);
-        CheckBox defeated = findViewById(R.id.chkDefeated);
 
-        String monsterName = getIntent().getStringExtra("name");
-        int monsterLevel = getIntent().getIntExtra("level", 1);
-        int iconResId = getIntent().getIntExtra("iconResId", R.mipmap.ic_launcher);
-
-        img.setImageResource(iconResId);
-        name.setText(monsterName != null ? monsterName : "Monster");
-        level.setText("Nivel " + monsterLevel);
-
-        // Por ahora no guarda en BD; luego lo persistimos.
-        defeated.setChecked(false);
-    }
-
-    // Flecha de “up” en la action bar
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        // Datos de prueba (luego vendrán por Intent)
+        name.setText("Goblin");
+        level.setText("Nivel 3");
     }
 }
