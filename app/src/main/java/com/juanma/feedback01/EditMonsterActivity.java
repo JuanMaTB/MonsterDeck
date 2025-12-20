8b985c5decd5dfb8315a14511bad75f872d8715b
Juan Manuel Torrado <jm.torrado.borrachero@gmail.com>
2025-12-20 12:30:59 +0100

    menu editar con acerca de

--- app/src/main/java/com/juanma/feedback01/EditMonsterActivity.java
+++ app/src/main/java/com/juanma/feedback01/EditMonsterActivity.java
@@ -2,6 +2,8 @@ package com.juanma.feedback01;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -9,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditMonsterActivity extends AppCompatActivity {
@@ -71,6 +74,52 @@ public class EditMonsterActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         finish(); // vuelve a la pantalla anterior
         return true;
+    }
+
+    // -------- MENÚ --------
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // En editar: solo dejamos "Acerca de"
+        MenuItem add = menu.findItem(R.id.action_add);
+        MenuItem edit = menu.findItem(R.id.action_edit);
+        MenuItem delete = menu.findItem(R.id.action_delete);
+        MenuItem filter = menu.findItem(R.id.action_filter_defeated);
+
+        if (add != null) add.setVisible(false);
+        if (edit != null) edit.setVisible(false);
+        if (delete != null) delete.setVisible(false);
+        if (filter != null) filter.setVisible(false);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        if (id == R.id.action_about) {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.about_title)
+                    .setMessage(R.string.about_message)
+                    .setPositiveButton("OK", null)
+                    .show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void saveAndExit() {
